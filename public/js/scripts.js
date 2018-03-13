9b373c89c9818e88bdbf17071f33abaea11abdb7
Casey Dallavalle <cbdallavalle@gmail.com>
2018-03-13 14:33:03 -0600

    Prepend Projects Projects stored in apps.local are prepended onto the page when the document loads. On the fetch request, express returns an object of the projects and palettes. The projects are looped through to return the HTML of the project name, and the palettes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,15 @@
+let currentColors = [];
+
+const generateColors = () => {
+  currentColors = [];
+  const displays = $('.palette');
+  $( '.palette' ).each(index => {
+    const randomHex = getRandomHex(['#']).join('');
+    currentColors.push(randomHex);
+    getSVG(`#${displays[index].id}`, randomHex);
+  }); 
+}
+
 const getRandomHex = (array) => {
   const hexValues = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
   const randomHex = Math.floor(Math.random() * hexValues.length);
@@ -8,17 +20,59 @@ const getRandomHex = (array) => {
   return getRandomHex(array);
 }
 
-const generateColors = () => {
-  const displays = $('.palette');
-  $( '.palette' ).each(index => {
-    const randomHex = getRandomHex(['#']).join('');
-    getSVG(`#${displays[index].id}`, randomHex);
-  }); 
-}
-
 const getSVG = (id, color) => {
   const svgs = $(`${id}`).children();
   svgs[0].getSVGDocument().getElementById("paint-color").setAttribute("style", `fill:${color}`);
   svgs[1].getSVGDocument().getElementById("brush-color").setAttribute("style", `fill:${color}`);
+}
 
+const getProjects = async () => {
+  const response = await fetch('/api/v1/projects');
+  const json = await response.json();
+  appendProjects(json);
+}
+
+const appendProjects = ({allProjects, allPalettes}) => {
+  const projectsToPrepend = allProjects.map( project => {
+    const palettes = getPalettes(project.id, allPalettes)
+    const palettesToPrepend = createPaletteHTML(palettes).join('');
+    return (`
+      <article class="project">
+        <h3>${project.name}</h3>
+        <div class="project-palettes">
+          ${palettesToPrepend}
+          <p><i class="fas fa-trash-alt"></i></p>
+        </div>
+      </article>
+    `)
+  })
+
+  $('.projects-cont').prepend(`${projectsToPrepend}`);
+}
+
+const getPalettes = (projectId, allPalettes) => {
+  return allPalettes.filter( (palette) => palette.project_id === projectId );
+}
+
+const createPaletteHTML = (palettes) => {
+  return palettes.map( palette => {
+    const divColors = getColors(palette.colors).join('');
+    return (`
+      <p>${palette.name}</p>
+      ${divColors}
+    `)
+  })
+}
+
+const getColors = (colors) => {
+  return colors.map( color => {
+    return (`
+      <div class="project-colors" id="color-1" role="colors" style="background-color:${color};"></div>
+    `)
+  })
+}
+
+const saveProject = (e) => {
+  e.preventDefault();
+  //post here to projects in database
 }
--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.locals.projects = [
+  { id: 1, name: 'first' },
+  { id: 2, name: 'second' },
+  { id: 3, name: 'third' }
+];
+
+app.locals.palettes = [
+  { id: 1, name: 'pinks', project_id: 1, colors: ['#FF89FF', '#F60FFF', '#AFF9FF', '#FBF0FF', '#CF9FFF'] },
+  { id: 2, name: 'blues', project_id: 1, colors: ['#FF89FF', '#F60FFF', '#AFF9FF', '#FBF0FF', '#CF9FFF'] },
+  { id: 3, name: 'blues', project_id: 2, colors: ['#FF89FF', '#F60FFF', '#AFF9FF', '#FBF0FF', '#CF9FFF'] }
+];
+
 //allows the server to receive a request from the user's port. If none exists, default to 3000
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
@@ -10,8 +22,20 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.use(express.static('public'))
 
-app.get('/', (request, response) => {
-  
+app.get('/api/v1/projects', (request, response) => {
+  const allProjects = app.locals.projects;
+  const allPalettes = app.locals.palettes;
+  return response.status(200).json({allProjects, allPalettes});
+})
+
+app.post('/api/v1/projects', (request, response) => {
+  const id = Date.now();
+  const information = request.body;
+
+  app.locals.projects.push({ id, ...information });
+  response.status(201).json({ id, ...information });
+  //push into 
+  //response 201
 })
 
 
